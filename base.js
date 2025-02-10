757def5bd71e36de83aeba8515a5e53b7b79ee82
Spencer Magnusson <semagnum@gmail.com>
2025-02-10 10:25:57 -0700

    Add service worker for offline caching

---
+++ base.js
@@ -0,0 +1,10 @@
+// make sure service workers are supported
+
+if('serviceWorker' in navigator){
+    window.addEventListener('load', () =>{
+      navigator.serviceWorker
+       .register('sw_pages.js')
+       .then(reg => console.log('Service Worker: Registered'))
+       .catch(err => console.log(`Service Worker: Error: ${err}`));
+    })
+  }
---
+++ sw_pages.js
@@ -0,0 +1,59 @@
+const cacheName = 'v1'; //Cache version name
+
+const cacheAssets = [ // list of files to cache
+    'index.html',
+    'index.css',
+
+    'qr/index.html',
+    'qr/qr.png',
+    
+    'assets/bcon.png',
+    'assets/bio.png',
+    'assets/blendermarketlogo.png',
+    'assets/extensions.png',
+    'assets/magnusson-192.png',
+    'assets/pixelfed-icon.png',
+    'assets/utility-course-badge.png',
+    
+    'base.css',
+    'favicon.ico',
+];
+
+
+self.addEventListener('install', e => {
+    console.log('Service Worker: Installed');
+    e.waitUntil(
+        caches
+        .open(cacheName)
+        .then(cache => {
+            console.log('Service Worker: Caching Files');
+            cache.addAll(cacheAssets);
+        })
+        .then(() => self.skipWaiting())
+    );
+  });
+  
+  
+  //Call install event
+  self.addEventListener('activate', e => {
+    console.log('Service Worker: Activated');
+    // Remove unwanted caches
+    e.waitUntil(
+      caches.keys().then(cacheNames => {
+          return Promise.all(
+              cacheNames.map(cache =>{
+                  if(cache !== cacheName){
+                      console.log('Service Worker: Clearing Old Cache');
+                      return caches.delete(cache);
+                  }
+              })
+          )
+      })
+    );
+  });
+
+  //Call fetch Event
+  self.addEventListener('fetch', e => {
+      console.log('Service Worker: Fetching');
+      e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  });
